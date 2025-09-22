396e3db39639e6c70c1b879a9d06c876d0454bef
Sharul <sharulsudhakaran@gmail.com>
2025-09-22 23:01:41 +0530

    removed outprocessing

--- outprocessing.py
+++
@@ -1,4 +0,0 @@
-from langchain.schema import HumanMessage,AIMessage
-text={'messages': [HumanMessage(content='write a simple factorial program in any language')], 'plan': AIMessage(content="Here's a development plan for a simple factorial program:"),'code': AIMessage(content='python code')}
-
-print(text['code'].content)
